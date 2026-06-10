e83cd6011d59b8632dbb38da5ce67486df390478
j92210137-hash <j92210137@gmail.com>
2026-06-10 15:54:44 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Wait lowk form1la made me",
         // Activity type number (0 = Playing).
         type: 0, 
       },
