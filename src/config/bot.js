40fa205eb42ff62145235bef4ac5455e931ca1d2
j92210137-hash <j92210137@gmail.com>
2026-06-11 02:26:28 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -20,14 +20,14 @@ export const botConfig = {
     // 1 = Streaming
     // 2 = Listening
     // 3 = Watching
-    // 4 = Gooning
+    // 4 = Custom
     // 5 = Competing
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Wait lowk form1la made me",
         // Activity type number (0 = Playing).
-        type: 4, 
+        type: 1, 
       },
     ],
   },
